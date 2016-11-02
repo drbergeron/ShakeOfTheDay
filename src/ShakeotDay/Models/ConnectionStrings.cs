3dd8b00a751983c9c4477b0755978c418508a888
drbergeron <chadabergeron@gmail.com>
2016-11-01 23:43:55 -0400

    initial commit

---
+++ src/ShakeotDay/Models/ConnectionStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShakeotDay.Models
+{
+    public class ConnectionStrings
+    {
+        public ConnectionStrings() { }
+        public string DefaultConnection { get; set; }
+    }
+}
---
+++ src/ShakeotDay/Models/Game/Dice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShakeotDay.Models
+{
+    public class Dice 
+    {
+        private Random roll;
+        private int _value;
+       public Dice()
+        {
+            roll = new Random();
+            value = 0;
+        }
+
+        public int value { get { return _value; } private set { _value = value; } }
+
+        public int Roll()
+        {
+            _value = roll.Next(1, 6);
+            return _value;
+        }
+    }
+}
